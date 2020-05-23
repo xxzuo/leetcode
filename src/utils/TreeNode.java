dc4dd4d81e14ad29f12249a504664681fabca7ab
xxzuo <1293378490@qq.com>
2020-05-23 23:18:41 +0800

    二叉树

---
+++ src/utils/TreeNode.java
@@ -0,0 +1,12 @@
+package utils;
+
+/**
+ * xxzuo
+ * 2020/5/23
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+}
